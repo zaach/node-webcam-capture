dd8736f3308928aa42aa8be1ccaf5bd8391b2420
Zachary Carter <zack.carter@gmail.com>
2014-01-12 23:06:28 -0800

    change option passing api, update readme

--- README.md
+++ README.md
@@ -2,11 +2,18 @@
 
 Capture frames from you webcam.
 
-This was part of a little weekend hack, so only it only works for OS X. Please send PRs for your platform or webcam setup.
+This was part of a little weekend hack, so it only works for OS X. Please send PRs for your platform or webcam setup.
 
 Cheers,
 [@zii](https://twitter.com/zii)
 
-# License
+## Example
+
+    var capture = require('webcam-capture');
+    var spawn = capture({ stdio: [null, process.stdin, null] });
+    // shortcut to write to a file
+    // var spawn = capture({ out: 'me.jpg' });
+
+## License
 
 MIT
--- index.js
+++ index.js
@@ -6,12 +6,11 @@ const executable = path.join(__dirname, 'build', 'Release', 'capture');
 function capture(options) {
   const out = options.out;
 
-  // Stream the captured image file, stdout, or custom streams.
-  const stdio = out ?
-    [null, fs.openSync(path.resolve(options.out), 'w'), process.stderr] :
-    (options.stdio || [null, process.stdout, process.stderr]);
+  if (out) {
+    options.stdio = [null, fs.openSync(path.resolve(out), 'w'), null];
+  }
 
-  return spawn(executable, [], { stdio: stdio });
+  return spawn(executable, [], options);
 }
 
 module.exports = capture;
